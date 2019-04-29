ab3b8026519c5c49afd3dca4cfd063e1a2e33a6b
RichardAndersen <richard.andersen@datastax.com>
2019-04-29 16:20:34 -0500

    Added compaction searchable class

--- DSEDiagnosticInsightsES/CompactionInformation.cs
+++ DSEDiagnosticInsightsES/CompactionInformation.cs
@@ -14,6 +14,32 @@ using DSEDiagnosticInsightsES;
 
 namespace DSEDiagnosticInsights
 {
+
+    public sealed class SSTableCompactionInformation
+    {
+        [JsonProperty("filename")]
+        public string Filename;
+
+        [JsonProperty("level")]
+        public int Level;
+
+        [JsonProperty("total_rows")]
+        public long TotalRows;
+
+        [JsonProperty("generation")]
+        public int Generation;
+
+        [JsonProperty("version")]
+        public string Version;
+
+        [JsonProperty("size_bytes")]
+        public long SizeBytes;
+
+        [JsonProperty("strategy")]
+        public string Strategy;
+
+    }
+
     [InsightESAttribute("dse.insights.event.compaction_ended", "dse.insights.event.compaction_ended-searchable-events")]
     public class CompactionEndedInformation : Insight<CompactionEndedInformation.CompactionInstance>
     {
@@ -50,35 +76,79 @@ namespace DSEDiagnosticInsights
             [JsonProperty("sstables")]
             public SSTableCompactionInformation[] SSTables;
         }
-
-        public sealed class SSTableCompactionInformation
-        {
-            [JsonProperty("filename")]
-            public string Filename;
-
-            [JsonProperty("level")]
-            public int Level;
-
-            [JsonProperty("total_rows")]
-            public long TotalRows;
-
-            [JsonProperty("generation")]
-            public int Generation;
-
-            [JsonProperty("version")]
-            public string Version;
-
-            [JsonProperty("size_bytes")]
-            public long SizeBytes;
-
-            [JsonProperty("strategy")]
-            public string Strategy;
-
-        }
     }
 
     [InsightESAttribute("dse.insights.event.compaction_started", "dse.insights.event.compaction_started-searchable-events")]
     public sealed class CompactionStartedInformation : Insight<CompactionEndedInformation.CompactionInstance>
     {
+    }
+
+    [InsightESAttribute("dse-compaction-events", "dse-compaction-events-searchable-events")]
+    public sealed class DseCompaction : Insight
+    {
+        [JsonProperty("id")]
+        public Guid Id;
+
+        [JsonProperty("type")]
+        public string Type;
+        
+        [JsonProperty("total_bytes")]
+        public long TotalBytes;
+
+        [JsonProperty("is_stop_requested")]
+        public bool IsStopRequested;
+
+        [JsonProperty("started_timestamp")]
+        public long StartedTimestamp;
+
+        [JsonIgnore]
+        public DateTimeOffset StartedTimeUTC
+        {
+            get
+            {
+                return DateTimeOffset.FromUnixTimeMilliseconds(this.StartedTimestamp);
+            }
+        }
+
+        [JsonProperty("started_total_sstable_size_bytes")]
+        public long StartedTotalSstableSizeBytes;
+
+        [JsonProperty("started_sstables")]
+        public SSTableCompactionInformation[] StartedSsTables;
+    
+        [JsonProperty("ended_timestamp")]
+        public long EndedTimestamp;
+
+        [JsonIgnore]
+        public DateTimeOffset EndedTimeUTC
+        {
+            get
+            {
+                return DateTimeOffset.FromUnixTimeMilliseconds(this.EndedTimestamp);
+            }
+        }
+
+        [JsonProperty("ended_total_sstable_size_bytes")]
+        public long EndedTotalSstableSizeBytes;
+
+        [JsonProperty("ended_total_source_cql_rows")]
+        public int EndedTotalSourceCqlRows;
+        
+        [JsonProperty("ended_sstables")]
+        public SSTableCompactionInformation[] EndedSsTables;
+
+        [JsonIgnore]
+        public TimeSpan Duration
+        {
+            get
+            {
+                return this.EndedTimeUTC - this.StartedTimeUTC;
+            }
+        }
+
+        public override object GetData()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
