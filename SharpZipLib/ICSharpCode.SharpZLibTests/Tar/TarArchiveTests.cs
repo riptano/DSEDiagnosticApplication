f679d6e0cf437ffe8106540f7a6ebdaad29b2b91
RichardAndersen <richard.andersen@datastax.com>
2019-12-12 15:23:50 -0700

    Uploaded Compression Lib

---
+++ SharpZipLib/ICSharpCode.SharpZLibTests/Tar/TarArchiveTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ICSharpCode.SharpZipLib.Tar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ICSharpCode.SharpZipLib.Tar.Tests
+{
+    [TestClass()]
+    public class TarArchiveTests
+    {
+        [TestMethod()]
+        public void ExtractContentsTest()
+        {
+            using (FileStream stream = new FileStream(@"E:\Libraries\Projects\DataStax\Diag-Customer\CapOne\10.186.71.169.tar", FileMode.Open, FileAccess.Read))
+            using (var gzipStream = new ICSharpCode.SharpZipLib.GZip.GZipInputStream(stream))
+            using (var tarArchive = ICSharpCode.SharpZipLib.Tar.TarArchive.CreateInputTarArchive(gzipStream))
+            {
+                tarArchive.FileOverwriteOption = TarArchive.FileOverWriteOptions.Always;
+                tarArchive.RestoreDateTimeOnExtract = true;
+                tarArchive.ExtractContents(@"E:\Libraries\Projects\DataStax\Diag-Customer\CapOne\");
+            }
+        }
+    }
+}
