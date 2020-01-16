c1e96923bbfbb33678b54409e16e21efa760ff4f
Richard Andersen <richard.andersen@datastax.com>
2020-01-15 17:30:19 -0700

    Added support for docker

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+FROM mcr.microsoft.com/dotnet/core/runtime:2.2
+COPY ./publish/linux-x64/DSEDiagnosticConsoleApplication.Core app/
---
+++ HCMSBuild-Publish-linux.sh
@@ -0,0 +1,1 @@
+dotnet publish ./DSEDiagnosticConsoleApplication.Core/ --framework netcoreapp2.2 --runtime linux-x64 --self-contained -o ./publish/linux-x64/DSEDiagnosticConsoleApplication.Core -c Release-NoRepro
